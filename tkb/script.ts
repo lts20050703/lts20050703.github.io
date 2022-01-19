64e96179085a27b05c3f00d2c9212b54b4fb8d40
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-01-19 19:48:21 +0700

    2022.01.19.2 +the-duc, -bg, rework index page and tkb page

--- tkb/script.ts
+++ tkb/script.ts
@@ -1,175 +1,41 @@
-const isOnlines: { [key: number]: { [key: number]: { [key: number]: boolean } } } = {
-	0: {
-		2: { 0: true, 1: false },
-		3: { 0: true },
-		4: { 0: false, 1: true },
-		5: { 0: false },
-		6: { 0: true, 1: false },
-		7: { 0: true }
-	},
-	1: {
-		2: { 0: false, 1: true },
-		3: { 0: false },
-		// 4: { 0: true, 1: false },
-		// 5: { 0: true },
-		// 6: { 0: false, 1: true },
-		// 7: { 0: false }
-		4: { 0: true, 1: true },
-		5: { 0: true },
-		6: { 0: true, 1: true },
-		7: { 0: true }
-	}
-}
-
-const tkb: { [key: number]: { [key: number]: { [key: number]: string } } } = {
-	2: {
-		0: { 1: "Chào cờ", 2: "Sinh hoạt lớp", 3: "Hình", 4: "Hình", 5: "Sinh" },
-		1: { 2: "Hóa", 3: "Hóa", 4: "GDCD", 5: "GDQP" }
-	},
-	4: { 0: { 1: "Văn", 2: "Văn", 3: "Tin", 4: "Tin", 5: "Tin" }, 1: { 3: "Anh", 4: "Anh", 5: "Anh" } },
-	5: { 0: { 1: "Địa", 2: "Thể dục", 3: "Thể dục", 4: "Lý", 5: "Lý" } },
-	6: { 0: { 1: "Giải tích", 2: "Giải tích", 3: "Giải tích", 4: "Văn", 5: "Văn" }, 1: { 2: "Sử", 3: "Lý", 4: "Lý" } },
-	7: { 0: { 1: "Hóa", 2: "Hóa", 3: "Công nghệ", 4: "Công nghệ", 5: "Hình" } }
-}
-
-const date = new Date()
-
 const paragraphs = document.getElementsByTagName("p")
+
+const bool = Math.floor((new Date().getTime() - new Date().getTime()) / (7 * 24 * 60 * 60 * 1000)) % 2 == 1
+
+const green = "#0f0", red = "#f00"
 
 for (let i = 0; i < paragraphs.length; i++) {
 	const p = paragraphs[i]
 	if (!p) continue
-	switch (p.id) {
-		case "00":
-			p.innerHTML = `Thứ ${date.getDay() + 1}`
+	switch (p.className) {
+		case "s2":
+			p.style.color = bool ? red : green
 			break
-		case "10": {
-			const isOnline = isOnlines[since_2022_01_17("w") % 2]?.[(since_2022_01_17("d") + 1) % 7 + 1]?.[0]
-			p.innerHTML = `${isOnline === undefined ? "." : isOnline ? "Online" : "Offline"}`
+		case "c2":
+			p.style.color = bool ? green : red
 			break
-		}
-		case "20": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[0]?.[1] || "."
+		case "s3":
+			p.style.color = bool ? red : green
 			break
-		}
-		case "21": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[0]?.[2] || "."
+		case "s4":
+			p.style.color = bool ? green : red
 			break
-		}
-		case "22": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[0]?.[3] || "."
+		case "c4":
+			p.style.color = bool ? red : green
 			break
-		}
-		case "23": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[0]?.[4] || "."
+		case "s5":
+			p.style.color = bool ? green : red
 			break
-		}
-		case "24": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[0]?.[5] || "."
+		case "s6":
+			p.style.color = bool ? red : green
 			break
-		}
-		case "30": {
-			const isOnline = isOnlines[since_2022_01_17("w") % 2]?.[(since_2022_01_17("d") + 1) % 7 + 1]?.[1]
-			p.innerHTML = `${isOnline === undefined ? "." : isOnline ? "Online" : "Offline"}`
+		case "c6":
+			p.style.color = bool ? green : red
 			break
-		}
-		case "40": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[1]?.[1] || "."
+		case "s7":
+			p.style.color = bool ? red : green
 			break
-		}
-		case "41": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[1]?.[2] || "."
-			break
-		}
-		case "42": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[1]?.[3] || "."
-			break
-		}
-		case "43": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[1]?.[4] || "."
-			break
-		}
-		case "44": {
-			p.innerHTML = tkb[date.getDay() + 1]?.[1]?.[5] || "."
-			break
-		}
-		case "50": {
-			p.innerHTML = `Thứ ${date.getDay() + 2}`
-			break
-		}
-		case "60": {
-			const isOnline =
-				isOnlines[since_2022_01_17("w", new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)) % 2]
-					?.[(since_2022_01_17("d") + 2) % 7 + 1]?.[0]
-			p.innerHTML = `${isOnline === undefined ? "." : isOnline ? "Online" : "Offline"}`
-			break
-		}
-		case "70": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[0]?.[1] || "."
-			break
-		}
-		case "71": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[0]?.[2] || "."
-			break
-		}
-		case "72": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[0]?.[3] || "."
-			break
-		}
-		case "73": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[0]?.[4] || "."
-			break
-		}
-		case "74": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[0]?.[5] || "."
-			break
-		}
-		case "80": {
-			const isOnline =
-				isOnlines[since_2022_01_17("w", new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)) % 2]
-					?.[(since_2022_01_17("d") + 2) % 7 + 1]?.[1]
-			p.innerHTML = `${isOnline === undefined ? "." : isOnline ? "Online" : "Offline"}`
-			break
-		}
-		case "90": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[1]?.[1] || "."
-			break
-		}
-		case "91": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[1]?.[2] || "."
-			break
-		}
-		case "92": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[1]?.[3] || "."
-			break
-		}
-		case "93": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[1]?.[4] || "."
-			break
-		}
-		case "94": {
-			p.innerHTML = tkb[date.getDay() + 2]?.[1]?.[5] || "."
-			break
-		}
 	}
 }
+
 export {}
-
-function since_2022_01_17(type: "w" | "d", d: Date = date): number {
-	switch (type) {
-		case "w":
-			return Math.floor((d.getTime() - new Date(2022, 0, 17).getTime()) / ms("w"))
-		case "d":
-			return Math.floor((d.getTime() - new Date(2022, 0, 17).getTime()) / ms("d"))
-	}
-}
-
-function ms(raw: "w" | "d") {
-	const s = 1000, m = 60 * s, h = 60 * m, d = 24 * h, w = 7 * d
-	switch (raw) {
-		case "d":
-			return d
-		case "w":
-			return w
-	}
-}
