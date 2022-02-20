d9bf60d177972b453204fec45018b7af6b5d95fb
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-02-20 17:55:08 +0700

    fix vite not building recursively

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
+import { resolve } from "path"
 import { defineConfig } from "vite"
+
 export default defineConfig({
 	base: "",
 	root: "src",
 	build: {
 		outDir: "../dist",
 		polyfillModulePreload: false,
-		emptyOutDir: true
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				anh: resolve(__dirname, "src/anh/index.html"),
+				celeste: resolve(__dirname, "src/celeste/index.html"),
+				"cong-nghe": resolve(__dirname, "src/cong-nghe/index.html"),
+				dia: resolve(__dirname, "src/dia/index.html"),
+				gdcd: resolve(__dirname, "src/gdcd/index.html"),
+				gdqp: resolve(__dirname, "src/gdqp/index.html"),
+				"giai-tich": resolve(__dirname, "src/giai-tich/index.html"),
+				hinh: resolve(__dirname, "src/hinh/index.html"),
+				hoa: resolve(__dirname, "src/hoa/index.html"),
+				ly: resolve(__dirname, "src/ly/index.html"),
+				main: resolve(__dirname, "src/index.html"),
+				ptnk: resolve(__dirname, "src/ptnk/index.html"),
+				sinh: resolve(__dirname, "src/sinh/index.html"),
+				su: resolve(__dirname, "src/su/index.html"),
+				"the-duc": resolve(__dirname, "src/the-duc/index.html"),
+				tin: resolve(__dirname, "src/tin/index.html"),
+				tkb: resolve(__dirname, "src/tkb/index.html"),
+				van: resolve(__dirname, "src/van/index.html")
+			}
+		}
 	}
 })
