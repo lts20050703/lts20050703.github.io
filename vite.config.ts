7842b7cf92434e40b72abc3cd9b8a12c456174df
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-04-04 15:23:27 +0700

    I swear I'm not inflating my commit count

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,22 @@ export default defineConfig({
 		emptyOutDir: true,
 		rollupOptions: {
 			input: {
-				anh: resolve(__dirname, "src/anh/index.html"),
-				"cong-nghe": resolve(__dirname, "src/cong-nghe/index.html"),
-				dia: resolve(__dirname, "src/dia/index.html"),
-				gdcd: resolve(__dirname, "src/gdcd/index.html"),
-				gdqp: resolve(__dirname, "src/gdqp/index.html"),
-				"giai-tich": resolve(__dirname, "src/giai-tich/index.html"),
-				hinh: resolve(__dirname, "src/hinh/index.html"),
-				hoa: resolve(__dirname, "src/hoa/index.html"),
-				ly: resolve(__dirname, "src/ly/index.html"),
-				main: resolve(__dirname, "src/index.html"),
-				ptnk: resolve(__dirname, "src/ptnk/index.html"),
-				sinh: resolve(__dirname, "src/sinh/index.html"),
-				su: resolve(__dirname, "src/su/index.html"),
-				"the-duc": resolve(__dirname, "src/the-duc/index.html"),
-				tin: resolve(__dirname, "src/tin/index.html"),
-				van: resolve(__dirname, "src/van/index.html")
+				anh: resolve(__dirname, "./src/anh/index.html"),
+				"cong-nghe": resolve(__dirname, "./src/cong-nghe/index.html"),
+				dia: resolve(__dirname, "./src/dia/index.html"),
+				gdcd: resolve(__dirname, "./src/gdcd/index.html"),
+				gdqp: resolve(__dirname, "./src/gdqp/index.html"),
+				"giai-tich": resolve(__dirname, "./src/giai-tich/index.html"),
+				hinh: resolve(__dirname, "./src/hinh/index.html"),
+				hoa: resolve(__dirname, "./src/hoa/index.html"),
+				ly: resolve(__dirname, "./src/ly/index.html"),
+				main: resolve(__dirname, "./src/index.html"),
+				ptnk: resolve(__dirname, "./src/ptnk/index.html"),
+				sinh: resolve(__dirname, "./src/sinh/index.html"),
+				su: resolve(__dirname, "./src/su/index.html"),
+				"the-duc": resolve(__dirname, "./src/the-duc/index.html"),
+				tin: resolve(__dirname, "./src/tin/index.html"),
+				van: resolve(__dirname, "./src/van/index.html")
 			}
 		}
 	}
