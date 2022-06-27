2dd57cf1042c94bbc8ec12257faedbb273f17c34
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-06-27 16:13:13 +0700

    update links

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { resolve } from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	root: "./src/",
-	build: { rollupOptions: { input: { main: resolve(__dirname, "./src/index.html") } } }
+	root: "src",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "src/index.html")
+			}
+		}
+	}
 })
