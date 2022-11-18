67d1dc09bbb2335e4dc83045bad6d3092e51b728
lts20050703 <lts20050703@gmail.com>
2022-11-18 20:05:22 +0700

    2022.11.18.1

--- _app/immutable/chunks/1-8a6883ce.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-91823198.js";export{t as component};
--- _app/immutable/chunks/2-6c3a2bea.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-93a0c81c.js";export{t as component};
