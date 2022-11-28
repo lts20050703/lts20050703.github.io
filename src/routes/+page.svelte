bae7de638ccb42c229efdc4df347852664d500ba
lts20050703 <lts20050703@gmail.com>
2022-11-28 11:53:40 +0700

    responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,29 @@
 </svelte:head>
 
 <div class="flex flex-col items-center justify-center">
-	<span class="text-3xl">LTS20050703</span>
-	<a href="https://antisoftware.club/" class="btn btn-link btn-lg text-3xl gap-2"
+	<span class="text-3xl gap-2">LTS20050703</span>
+	<a href="https://antisoftware.club/" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./assc.png" alt="anti software software club icon" class="h-12" /> anti software software
 		club</a
 	>
-	<a href="https://cohost.org/lts20050703" class="btn btn-link btn-lg text-3xl gap-2"
+	<a href="https://cohost.org/lts20050703" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./cohost.ico" alt="cohost! icon" class="h-12" /> cohost!</a
 	>
-	<a href="https://mastodon.social/@lts20050703" class="btn btn-link btn-lg text-3xl gap-2"
+	<a href="https://mastodon.social/@lts20050703" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./mastodon.ico" alt="Mastodon icon" class="h-12" /> Mastodon</a
 	>
-	<a href="https://sfconservancy.org/GiveUpGitHub/" class="btn btn-link btn-lg text-3xl gap-2"
+	<a
+		href="https://sfconservancy.org/GiveUpGitHub/"
+		class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./sfc.ico" alt="software freedom conservancy icon" class="h-12" /> Give Up GitHub</a
 	>
-	<a href="https://wist.lts20050703.repl.co" class="btn btn-link btn-lg text-3xl gap-2"
+	<a href="https://wist.lts20050703.repl.co" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./wist.png" alt="WIST icon" class="h-12" /> WIST</a
 	>
-	<a href="https://lts20050703.codeberg.page/timer" class="btn btn-link btn-lg text-3xl gap-2">
+	<a
+		href="https://lts20050703.codeberg.page/timer"
+		class="btn btn-link btn-lg gap-2 text-3xl flex-1"
+	>
 		<img src="./favicon.png" alt="Long Term Support icon" class="h-12" />
 		Timer loosely based on BOTW Stamina Wheel
 	</a>
