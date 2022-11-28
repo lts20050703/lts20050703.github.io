1cc7c9205d163e9ccb2fcd7a61b1d288e63619a5
lts20050703 <lts20050703@gmail.com>
2022-11-28 23:50:36 +0700

    add irx, remove assc and sfc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,29 @@
 </svelte:head>
 
 <div class="flex flex-col items-center justify-center">
-	<div class="text-3xl flex-1 gap-2">LTS20050703</div>
-	<a href="https://antisoftware.club/" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./assc.png" alt="anti software software club icon" class="h-12" /> anti software software
-		club</a
-	>
+	<div class="text-3xl flex-1 gap-2 flex flex-row pt-3">
+		<img src="./favicon.png" alt="Long Term Support Icon" class="h-12" />LTS20050703
+	</div>
 	<a href="https://cohost.org/lts20050703" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./cohost.ico" alt="cohost! icon" class="h-12" /> cohost!</a
 	>
 	<a href="https://mastodon.social/@lts20050703" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
 		><img src="./mastodon.ico" alt="Mastodon icon" class="h-12" /> Mastodon</a
 	>
-	<a
-		href="https://sfconservancy.org/GiveUpGitHub/"
-		class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./sfc.ico" alt="software freedom conservancy icon" class="h-12" /> Give Up GitHub</a
-	>
-	<a href="./wist/" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./wist.png" alt="WIST icon" class="h-12" /> WIST</a
-	>
 	<a href="./timer/" class="btn btn-link btn-lg gap-2 text-3xl flex-1">
 		<img src="./favicon.png" alt="Long Term Support icon" class="h-12" />
 		Timer loosely based on BOTW Stamina Wheel
 	</a>
+	<a href="https://wist.lts20050703.repl.co" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
+		><img src="./e5y.png" alt="WIST icon" class="h-12" />WIST</a
+	>
+	<div class="gap-2 text-3xl flex-1 flex flex-row pt-10">
+		<img src="./irx.png" alt="In Real Experience icon" class="h-12" />IRX
+	</div>
+	<a href="./wist/" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
+		><img src="./wist.png" alt="WIST icon" class="h-12" /> WIST</a
+	>
+	<a href="https://codeberg.org/lts20050703/e5y" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
+		><img src="./e5y.png" alt="E5Y icon" class="h-12" /> E5Y</a
+	>
 </div>
