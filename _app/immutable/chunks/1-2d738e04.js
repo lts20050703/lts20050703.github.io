6bcfb86c3cb7c4e7f165c40e054ae0c054445137
lts20050703 <lts20050703@gmail.com>
2022-11-28 23:57:12 +0700

    fix wrong icon

---
+++ _app/immutable/chunks/1-2d738e04.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-31ee444c.js";export{t as component};
--- _app/immutable/chunks/2-cf64541a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-393ca4e8.js";export{t as component};
