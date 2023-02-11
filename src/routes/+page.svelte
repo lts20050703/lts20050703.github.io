e7eb67ce16251e5d2941895be92dda4619db08e1
lts20050703 <lts20050703@gmail.com>
2023-02-11 09:58:21 +0700

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,21 @@
 
 <div class="flex flex-col items-center justify-center">
 	<div class="text-3xl flex-1 gap-2 flex flex-row pt-3 text-white">
-		<img src="./favicon.png" alt="Long Term Support Icon" class="h-12" />LTS20050703
+		<img src="./favicon.png" alt="" class="h-12" />LTS20050703
 	</div>
 	<a href="https://cohost.org/lts20050703" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./cohost.ico" alt="cohost! icon" class="h-12" /> cohost!</a
+		><img src="./cohost.ico" alt="" class="h-12" />cohost!</a
 	>
 	<a href="https://mastodon.social/@lts20050703" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./mastodon.ico" alt="Mastodon icon" class="h-12" /> Mastodon</a
+		><img src="./mastodon.ico" alt="" class="h-12" />Mastodon</a
 	>
 	<div class="gap-2 text-3xl flex-1 flex flex-row pt-10 text-white">
-		<img src="./irx.png" alt="In Real Experience icon" class="h-12" />IRX
+		<img src="./irx.png" alt="" class="h-12" />In Real Xperience
 	</div>
 	<a href="https://wist.lts20050703.repl.co" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./wist.png" alt="WIST icon" class="h-12" /> WIST</a
+		><img src="./wist.png" alt="" class="h-12" />WIST</a
 	>
 	<a href="https://codeberg.org/lts20050703/e5y" class="btn btn-link btn-lg gap-2 text-3xl flex-1"
-		><img src="./e5y.png" alt="E5Y icon" class="h-12" /> E5Y</a
+		><img src="./e5y.png" alt="" class="h-12" />Entropy</a
 	>
 </div>
