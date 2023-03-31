5d265aa2663eb27e6cddcae14cb64fa05d817f81
lts20050703 <lts20050703@gmail.com>
2023-03-31 16:41:01 +0700

    temp: openclozes

--- src/routes/openclozes/+page.svelte
+++ src/routes/openclozes/+page.svelte
@@ -1,43 +1,16 @@
-<script lang="ts">
-	const input = `Mr. Jackson's father had just {passed away}. He had died of cancer. George had {sat up with} him during the last night at the hospital. Both men knew that the end was {drawing near} but the older man {kept on} talking cheerfully.
-"I am not unhappy," he told his son. "Sometimes life has been difficult, but I usually {face up to} its problems. When I was a student, I {tried out for} the football team and didn't meet with sueces. I {dropped out of} high school for a year after that. I {fell in with} some young men who were robbing people on the streets. We didn't use real guns. We {held} people {up} with wooden guns that looked real.`
-	let answers = input.split(/{|}/).filter((_, i) => i % 2 === 1)
-	let inputs = answers.map((_) => "")
-	let live = false
-	$: live, console.log(live)
-</script>
-
-<svelte:head>
-	<title>OPEN CLOZES</title>
-</svelte:head>
-
 <div class="w-screen flex items-center flex-col p-4">
 	<div class="text-white leading-loose xl:w-[48rem]">
-		{#each input.split(/{|}/) as text, i}
-			{#if i % 2 === 0}
-				{#if text.includes("\n")}
-					{text.split("\n")[0]}
-					<br />
-					{text.split("\n")[1]}
-				{:else}
-					{text}
-				{/if}
-			{:else}
-				<input
-					type="text"
-					class="input h-7 mx-1 p-1 {inputs[(i - 1) / 2] === '' || !live
-						? 'input-primary'
-						: inputs[(i - 1) / 2].toLowerCase().split(/ +/).join(' ').trim() ===
-						  answers[(i - 1) / 2]
-						? 'input-success'
-						: 'input-error'}"
-					bind:value={inputs[(i - 1) / 2]}
-				/>
-			{/if}
-		{/each}
+		<div class="text-red-500 text-center">WORK IN PROGRESS, NOT COMPLETED YET</div>
+		<br />
+		<div class="flex flex-row gap-4">
+			<a href="./0" class="btn btn-primary flex-1">0</a>
+			<a href="./1" class="btn btn-primary flex-1">1</a>
+			<a href="./2" class="btn btn-primary flex-1">2</a>
+			<a href="./3" class="btn btn-primary flex-1">3</a>
+		</div>
+		<br />
+		<div class="text-center">Changelog</div>
+		<div>2023.03.31@16:30 Add autosave (saved to localStorage)</div>
+		<div>2023.03.31@16:40 Add check answer, show answers, and clear button</div>
 	</div>
-</div>
-
-<div class="text-white fixed bottom-4 left-0 right-0 flex gap-4 justify-center">
-	Live update <input type="checkbox" class="checkbox checkbox-primary" bind:checked={live} />
 </div>
---
+++ src/routes/openclozes/1/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	const input = ``
+	let answers = input.split(/{|}/).filter((_, i) => i % 2 === 1)
+	let stored = localStorage.getItem("inputs1")?.split(",")
+	if (stored && stored.length < answers.length)
+		stored.push(...Array(answers.length - stored.length).fill(""))
+	let inputs = stored ?? answers.map((_) => "")
+	let live = Boolean(localStorage.getItem("live")) ?? false
+	$: inputs, localStorage.setItem("inputs1", inputs.join())
+	$: live, localStorage.setItem("live", live ? "true" : "false")
+	function clear() {
+		localStorage.removeItem("inputs1")
+		location.reload()
+	}
+
+	function check() {
+		live = true
+	}
+
+	function show_answer() {
+		inputs = answers
+	}
+</script>
+
+<svelte:head>
+	<title>OPEN CLOZES</title>
+</svelte:head>
+
+<div class="w-screen flex items-center flex-col p-4">
+	<div class="text-white leading-loose xl:w-[48rem]">
+		<div class="text-red-500 text-center">WORK IN PROGRESS, NOT COMPLETED YET</div>
+		<div class="text-white text-center flex flex-row gap-4 justify-center">
+			<a href="../" class="btn btn-primary btn-sm">Back</a>
+			<div>
+				Live update <input type="checkbox" class="checkbox checkbox-primary" bind:checked={live} />
+			</div>
+			<button class="btn btn-success btn-sm" on:click={check}>Check</button>
+			<button class="btn btn-warning btn-sm" on:click={show_answer}>Show answers</button>
+
+			<button class="btn btn-error btn-sm" on:click={clear}>Clear</button>
+		</div>
+		{#each input.split(/{|}/) as text, i}
+			{#if i % 2 === 0}
+				{#if text.includes("\n")}
+					{text.split("\n")[0]}
+					<br />
+					{text.split("\n")[1]}
+				{:else}
+					{text}
+				{/if}
+			{:else}
+				<input
+					type="text"
+					class="input h-7 mx-1 p-1 {inputs[(i - 1) / 2] === '' || !live
+						? 'input-primary'
+						: inputs[(i - 1) / 2].toLowerCase().split(/ +/).join(' ').trim() ===
+						  answers[(i - 1) / 2]
+						? 'input-success'
+						: 'input-error'}"
+					bind:value={inputs[(i - 1) / 2]}
+				/>
+			{/if}
+		{/each}
+	</div>
+</div>
