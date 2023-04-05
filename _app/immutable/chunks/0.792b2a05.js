169d53d7a083944a839a3e6861e75dd49d191b66
lts20050703 <lts20050703@gmail.com>
2023-04-05 08:04:29 +0700

    2023.04.04.2 supporting iphone 6 ugh

--- _app/immutable/chunks/0.792b2a05.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_layout.fdd757eb.js";import{default as t}from"../entry/_layout.svelte.fc1cf3b4.js";export{t as component,r as universal};
--- _app/immutable/chunks/1.5d4c4b2c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.333e78b8.js";export{t as component};
