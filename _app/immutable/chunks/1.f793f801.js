4c0d48b812792dab181f63bcd71789f6927935fe
lts20050703 <lts20050703@gmail.com>
2023-04-13 21:46:37 +0700

    WIP: 2023 stuff

--- _app/immutable/chunks/1.f793f801.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.53055077.js";export{t as component};
---
+++ _app/immutable/chunks/3.9223d8a0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/2023-page.svelte.d0a6070f.js";export{t as component};
