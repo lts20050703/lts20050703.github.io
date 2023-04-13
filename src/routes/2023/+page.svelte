0f98a78757878d226bad1d7246b51645ec75aec7
lts20050703 <lts20050703@gmail.com>
2023-04-13 21:45:49 +0700

    WIP: 2023 stuff

---
+++ src/routes/2023/+page.svelte
@@ -0,0 +1,18 @@
+<div class="w-full text-black dark:text-white flex flex-col items-center">
+	<span class="text-3xl text-center">SINH</span>
+	<span class="text-xl text-center">BÀI 24 CÁC BẰNG CHỨNG TIẾN HÓA</span>
+	<div class="flex flex-row">
+		<a
+			class="btn btn-primary"
+			href="https://azota.vn/de-thi/m515xy"
+			target="_blank"
+			rel="noopener noreferrer">AZOTA</a
+		>
+		<a
+			class="btn btn-primary"
+			href="/2023/sinh/SINH BÀI 24 CÁC BẰNG CHỨNG TIẾN HÓA.docx"
+			target="_blank"
+			rel="noopener noreferrer">WORD</a
+		>
+	</div>
+</div>
