9db57d8e56f926deeda2b67085efe93744b0ebba
lts20050703 <lts20050703@gmail.com>
2023-04-14 16:05:40 +0700

    WIP: SSR stuf

--- src/routes/2023/+page.svelte
+++ src/routes/2023/+page.svelte
@@ -1,18 +1,33 @@
-<div class="w-full text-black dark:text-white flex flex-col items-center">
+<div class="w-full text-black dark:text-white flex flex-col items-center gap-1">
 	<span class="text-3xl text-center">SINH</span>
 	<span class="text-xl text-center">BÀI 24 CÁC BẰNG CHỨNG TIẾN HÓA</span>
-	<div class="flex flex-row">
+	<div class="flex flex-row gap-1">
 		<a
 			class="btn btn-primary"
 			href="https://azota.vn/de-thi/m515xy"
 			target="_blank"
 			rel="noopener noreferrer">AZOTA</a
 		>
+		<a class="btn btn-primary" href="./sinh/24" target="_blank" rel="noopener noreferrer">TEXT</a>
+	</div>
+	<span class="text-xl text-center">BÀI 35 36</span>
+	<div class="flex flex-row gap-1">
 		<a
 			class="btn btn-primary"
-			href="/2023/sinh/SINH BÀI 24 CÁC BẰNG CHỨNG TIẾN HÓA.docx"
+			href="https://azota.vn/de-thi/svobta"
 			target="_blank"
-			rel="noopener noreferrer">WORD</a
+			rel="noopener noreferrer">AZOTA</a
 		>
+		<a class="btn btn-primary" href="./sinh/3536" target="_blank" rel="noopener noreferrer">TEXT</a>
+	</div>
+	<span class="text-xl text-center">BÀI 37 38 CÁC ĐẶC TRƯNG CƠ BẢN CỦA QUẦN THỂ SINH VẬT</span>
+	<div class="flex flex-row gap-1">
+		<a
+			class="btn btn-primary"
+			href="https://azota.vn/de-thi/tsgwyw"
+			target="_blank"
+			rel="noopener noreferrer">AZOTA</a
+		>
+		<a class="btn btn-primary" href="./sinh/3738" target="_blank" rel="noopener noreferrer">TEXT</a>
 	</div>
 </div>
---
+++ src/routes/2023/sinh/3536/+page.svelte
@@ -0,0 +1,208 @@
+<div class="text-black dark:text-white">
+	<p>
+		<strong>Câu 1</strong>: Phát biểu nào sau đây là đúng khi nói về môi trường sống của sinh vật?
+	</p>
+	<p>
+		<strong>A.</strong> Môi trường là nơi sống của sinh vật, bao gồm tất cả các nhân tố vô sinh và hữu
+		sinh ở xung quanh sinh vật, có tác  động trực tiếp, gián tiếp tới sinh vật.
+	</p>
+	<p>
+		<strong>*B.</strong> Môi trường bao gồm tất cả các nhân tố ở xung quanh sinh vật, có tác động trực
+		tiếp, gián tiếp tới sinh vật, làm ảnh  hưởng tới sự tồn tại, sinh trưởng, phát triển và những hoạt
+		động khác của sinh vật.
+	</p>
+	<p>
+		<strong>C.</strong> Môi trường là nơi sống của sinh vật, bao gồm tất cả các nhân tố vô sinh xung
+		quanh sinh vật.
+	</p>
+	<p>
+		<strong>D.</strong> Môi trường là nơi sống của sinh vật, bao gồm tất cả các nhân tố hữu sinh xung
+		quanh sinh vật.
+	</p>
+	<p><strong>Câu 2</strong>: Môi trường sống của sinh vật gồm các loại môi trường:</p>
+	<p><strong>A.</strong> Đất - nước - không khí</p>
+	<p><strong>B.</strong> Đất - nước - không khí - sinh vật</p>
+	<p><strong>C.</strong> Đất - nước - không khí - trên cạn</p>
+	<p><strong>*D.</strong> Đất - nước - trên cạn - sinh vật</p>
+	<p>
+		<strong>Câu 3</strong>: Phát biểu nào là không đúng khi nói về sự tác động của các nhân tố sinh
+		thái lên sinh vật?
+	</p>
+	<p>
+		<strong>A.</strong> Cùng một lúc, tổ hợp các nhân tố sinh thái cùng tác động đồng thời lên sinh vật
+		tạo nên tác động tổng hợp
+	</p>
+	<p>
+		<strong>*B.</strong> Các loài khác nhau sẽ phản ứng như nhau với tác động như nhau của một nhân tố
+		sinh thái
+	</p>
+	<p>
+		<strong>C.</strong> Trong các giai đoạn khác nhau hay trạng thái sinh lí khác nhau thì cơ thể phản
+		ứng khác nhau với tác động như  nhau của cùng một nhân tố sinh thái
+	</p>
+	<p>
+		<strong>D.</strong> Các nhân tố sinh thái khi tác động lên cơ thể có thể gây tăng cường hoặc kìm
+		hãm nhau
+	</p>
+	<p><strong>Câu 4</strong>: Khoảng thuận lợi là khoảng giá trị của các nhân tố sinh thái mà</p>
+	<p><strong>A.</strong> ở đó sinh vật sinh sản tốt nhất.</p>
+	<p><strong>*B.</strong> ở mức phù hợp nhất để sinh vật thực hiện chức năng sống tốt nhất.</p>
+	<p><strong>C.</strong> giúp sinh vật chống chịu tốt nhất với môi trường.</p>
+	<p><strong>D.</strong> ở đó sinh vật sinh trưởng, phát triển tốt nhất.</p>
+	<p><strong>Câu 5</strong>: Giới hạn sinh thái là gì?</p>
+	<p>
+		<strong>A.</strong> Là giới hạn chịu đựng của sinh vật đối với nhiều nhân tố sinh thái của môi trường,
+		nằm ngoài giới hạn sinh thái, sinh  vật không thể tồn tại được.
+	</p>
+	<p>
+		<strong>B.</strong> Là giới hạn chịu đựng của sinh vật đối với một hoặc một số nhân tố sinh thái
+		của môi trường, nằm ngoài giới hạn  sinh thái, sinh vật không thể tồn tại được.
+	</p>
+	<p>
+		<strong>*C.</strong> Là khoảng giá trị xác định của một số nhân tố sinh thái mà trong khoảng đó sinh
+		vật có thể tồn tại và phát triển theo  thời gian.
+	</p>
+	<p>
+		<strong>D.</strong> Là giới hạn chịu đựng của sinh vật đối với một số nhân tố sinh thái của môi trường;
+		nằm ngoài giới hạn sinh thái,  sinh vật không thể tồn tại được.
+	</p>
+	<p>
+		<strong>Câu 6</strong>: Khoảng giá trị xác định của một nhân tố sinh thái mà trong khoảng đó
+		sinh vật có thể tồn tại và phát triển ổn  định theo thời gian được gọi là:
+	</p>
+	<p><strong>A.</strong> Môi trường</p>
+	<p><strong>*B.</strong> Giới hạn sinh thái</p>
+	<p><strong>C.</strong> Ổ sinh thái</p>
+	<p><strong>D.</strong> Sinh cảnh</p>
+	<p><strong>Câu 7</strong>: Nhân tố sinh thái là</p>
+	<p>
+		<strong>*A.</strong> tất cả những nhân tố môi trường, ảnh hưởng trực tiếp hoặc gián tiếp tới đời
+		sống của sinh vật.
+	</p>
+	<p>
+		<strong>B.</strong> tất cả những nhân tố vật lí và hóa học của môi trường xung quanh sinh vật (nhân
+		tố vô sinh).
+	</p>
+	<p>
+		<strong>C.</strong> những mối quan hệ giữa một sinh vật (hoặc nhóm sinh vật) này với một sinh vật
+		(hoặc nhóm sinh vật) khác sống  xung quanh (nhân tố hữu sinh).
+	</p>
+	<p><strong>D.</strong> những tác động của con người đến môi trường.</p>
+	<p>
+		<strong>Câu 8</strong>: Trên một cây to, có nhiều loài chim sinh sống, có loài sống trên cao, có
+		loài sống dưới thấp, hình thành các...  khác nhau
+	</p>
+	<p><strong>A.</strong> Quần thể</p>
+	<p><strong>*B.</strong> Ổ sinh thái</p>
+	<p><strong>C.</strong> Quần xã</p>
+	<p><strong>D.</strong> Sinh cảnh</p>
+	<p><strong>Câu 9</strong>: Sinh vật có khả năng phân bố rộng trong trường hợp nào sau đây?</p>
+	<p><strong>A.</strong> Điểm gây chết thấp</p>
+	<p><strong>B.</strong> Khoảng thuận lợi rộng</p>
+	<p><strong>C.</strong> Khoảng chống chịu rộng</p>
+	<p><strong>*D.</strong> Ổ sinh thái rộng</p>
+	<p><strong>Câu 10</strong>: Trong các phát biểu sau:</p>
+	<p><strong>I. Ổ sinh thái của một loài là nơi ở của loài đó. </strong></p>
+	<p><strong>II. Ổ sinh thái đặc trưng cho loài. </strong></p>
+	<p>
+		<strong
+			>III. Các loài sống trong một sinh cảnh và cùng sử dụng một nguồn thức ăn thì chúng có xu
+			hướng phân li ổ sinh thái.  IV. Kích thước thức ăn, loại thức ăn của mỗi loài tạo nên các ổ
+			sinh thái về dinh dưỡng.
+		</strong>
+	</p>
+	<p>Có bao nhiêu phát biểu đúng khi nói về ổ sinh thái?</p>
+	<p><strong>A.</strong> 1.</p>
+	<p><strong>B.</strong> 2.</p>
+	<p><strong>*C.</strong> 3.</p>
+	<p><strong>D.</strong> 4.</p>
+	<p>
+		<strong>Câu 11</strong>: Phần lớn thực vật sống ở vùng nhiệt đới quang hợp tốt nhất ở nhiệt độ
+		20°C - 30°C, khi nhiệt độ xuống dưới  0°C và cao hơn 40°C cây trồng ngừng quang hợp. Trong các
+		phát biểu sau:
+	</p>
+	<p>1. 20°C - 30°C được gọi là giới hạn sinh thái.</p>
+	<p>2. 20°C - 30°C được gọi là khoảng thuận lợi.</p>
+	<p>3. 0°C - 40°C được gọi là giới hạn sinh thái về nhiệt độ.</p>
+	<p>4. 0°C - 40°C được gọi là khoảng chống chịu.</p>
+	<p>5. 0°C được gọi là giới hạn dưới, 40°C được gọi là giới hạn trên.</p>
+	<p>Có bao nhiêu phát biểu đúng?</p>
+	<p><strong>A.</strong> 1</p>
+	<p><strong>B.</strong> 2</p>
+	<p><strong>*C.</strong> 3</p>
+	<p><strong>D.</strong> 5</p>
+	<p><strong>Câu 12</strong>: Nhóm sinh vật nào sau đây chỉ gồm những loài biến nhiệt?</p>
+	<p><strong>A.</strong> Chuột đồng, chim sẻ, nhím.</p>
+	<p><strong>B.</strong> Dơi, vi khuẩn lam, nấm mèo.</p>
+	<p><strong>C.</strong> Cá voi, voi biển, chó biển, cá sấu.</p>
+	<p><strong>*D.</strong> Sâu bọ, tôm, ếch nhái, rùa, rắn, kỳ đà.</p>
+	<p>
+		<strong>Câu 13</strong>: Cho 4 loài có giới hạn trên, điểm cực thuận và giới hạn dưới về nhiệt
+		độ lần lượt là:  Loài 1: 15°, 33°C, 40°C
+	</p>
+	<p>Loài 2: 8°C, 20°C, 38°C</p>
+	<p>Loài 3: 29°C, 36°C, 50°C</p>
+	<p>Loài 4: 4°C, 16°C, 22°C</p>
+	<p>Giới hạn nhiệt độ rộng nhất thuộc về:</p>
+	<p><strong>*A.</strong> Loài 2</p>
+	<p><strong>B.</strong> Loài 1</p>
+	<p><strong>C.</strong> Loài 3</p>
+	<p><strong>D.</strong> Loài 4</p>
+	<p><strong>Câu 14</strong>: Nhân tố vô sinh bao gồm tất cả:</p>
+	<p><strong>*A.</strong> nhân tố vật lí, nhân tố hóa học của môi trường xung quanh sinh vật.</p>
+	<p><strong>B.</strong> tác động của các sinh vật khác lên cơ thể sinh vật.</p>
+	<p><strong>C.</strong> tác động trực tiếp hay gián tiếp của tự nhiên lên cơ thể sinh vật.</p>
+	<p><strong>D.</strong> các yếu tố sống của tự nhiên có ảnh hưởng đến cơ thể sinh vật.</p>
+	<p><strong>Câu 15</strong>: Hiện tượng cá thể tách ra khỏi nhóm:</p>
+	<p><strong>A.</strong> làm tăng khả năng cạnh tranh giữa các cá thể.</p>
+	<p><strong>B.</strong> làm tăng mức độ sinh sản.</p>
+	<p>
+		<strong>*C.</strong> làm giảm nhẹ cạnh tranh giữa các cá thể, hạn chế sự cạn kiệt nguồn thức ăn trong
+		vùng.
+	</p>
+	<p><strong>D.</strong> làm cho nguồn thức ăn cạn kiệt nhanh</p>
+	<p><strong>Câu 16</strong>: Nhóm cá thể nào dưới đây là một quần thể?</p>
+	<p><strong>A.</strong> Tập hợp cây cỏ ven bờ.</p>
+	<p><strong>*B.</strong> Đàn cá rô trong ao.</p>
+	<p><strong>C.</strong> Tập hợp thú trong rừng.</p>
+	<p><strong>D.</strong> Cây trong vườn.</p>
+	<p>
+		<strong>Câu 17</strong>: Hiện tượng nào sau đây là biểu hiện của mối quan hệ hỗ trợ cùng loài?
+	</p>
+	<p><strong>A.</strong> Cá mập con khi mới nở, sử dụng trứng chưa nở làm thức ăn.</p>
+	<p><strong>B.</strong> Động vật cùng loài ăn thịt lẫn nhau.</p>
+	<p><strong>C.</strong> Tỉa thưa tự nhiên ở thực vật.</p>
+	<p><strong>*D.</strong> Các cây thông mọc gần nhau, có rễ nối liền nhau.</p>
+	<p><strong>Câu 18</strong>: Quần thể là một tập hợp cá thể</p>
+	<p>
+		<strong>A.</strong> cùng loài, sống trong 1 khoảng không gian xác định, có khả năng sinh sản tạo
+		thế hệ mới.
+	</p>
+	<p>
+		<strong>B.</strong> khác loài, sống trong 1 khoảng không gian xác định vào một thời điểm xác định.
+	</p>
+	<p>
+		<strong>C.</strong> cùng loài, cùng sống trong 1 khoảng không gian xác định, vào một thời điểm xác
+		định.
+	</p>
+	<p>
+		<strong>*D.</strong> cùng loài, cùng sống trong 1 khoảng không gian xác định, vào một thời điểm xác
+		định, có khả năng sinh sản tạo thế  hệ mới.
+	</p>
+	<p>
+		<strong>Câu 19</strong>: Một số cây cùng loài sống gần nhau có hiện tượng rễ của chúng nối với
+		nhau (liền rễ). Hiện tượng này thể  hiện mối quan hệ
+	</p>
+	<p><strong>A.</strong> cạnh tranh cùng loài.</p>
+	<p><strong>B.</strong> hỗ trợ khác loài.</p>
+	<p><strong>C.</strong> cộng sinh.</p>
+	<p><strong>*D.</strong> hỗ trợ cùng loài.</p>
+	<p>
+		<strong>Câu 20</strong>: Nhóm sinh vật nào dưới đây có nhiệt độ cơ thể không biến đổi theo nhiệt
+		độ môi trường?
+	</p>
+	<p><strong>A.</strong> Lưỡng cư.</p>
+	<p><strong>B.</strong> Cá xương.</p>
+	<p><strong>*C.</strong> Chim.</p>
+	<p><strong>D.</strong> Bò sát.</p>
+</div>
