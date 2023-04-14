451bf102a9cf97141113a63142fb219c521d009e
lts20050703 <lts20050703@gmail.com>
2023-04-14 16:36:11 +0700

    WIP: 2023 stuff

--- src/routes/2023/+page.svelte
+++ src/routes/2023/+page.svelte
@@ -40,4 +40,16 @@
 		>
 		<a class="btn btn-primary" href="./sinh/39" target="_blank" rel="noopener noreferrer">TEXT</a>
 	</div>
+	<span class="text-xl text-center"
+		>BÀI 40 QUẦN XÃ SINH VẬT VÀ MỘT SỐ ĐẶC TRƯNG CƠ BẢN CỦA QUẦN XÃ SINH VẬT
+	</span>
+	<div class="flex flex-row gap-1">
+		<a
+			class="btn btn-primary"
+			href="https://azota.vn/de-thi/mzmncx"
+			target="_blank"
+			rel="noopener noreferrer">AZOTA</a
+		>
+		<a class="btn btn-primary" href="./sinh/40" target="_blank" rel="noopener noreferrer">TEXT</a>
+	</div>
 </div>
