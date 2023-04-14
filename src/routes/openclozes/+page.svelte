22a94adc702878b20229b42ed51c23bcdec9d28d
lts20050703 <lts20050703@gmail.com>
2023-04-14 15:28:18 +0700

    WIP: SSR stuf

--- src/routes/openclozes/+page.svelte
+++ src/routes/openclozes/+page.svelte
@@ -5,7 +5,9 @@
 <div class="absolute top-0 left-0 right-0 flex items-center flex-col p-4">
 	<div class="text-black dark:text-white leading-loose w-full">
 		<div class="flex flex-col sm:grid sm:grid-cols-2 lg:flex lg:flex-row gap-4">
-			<a href="./0" class="btn btn-primary flex-1">AN OLD MAN LOOKED BACK ON HIS LIFE</a>
+			<a href="./an-old-man-looked-back-on-his-life" class="btn btn-primary flex-1"
+				>AN OLD MAN LOOKED BACK ON HIS LIFE</a
+			>
 			<a href="./1" class="btn btn-primary flex-1">GETTING ALONG IN ONE'S SCHOOL WORK</a>
 			<a href="./2" class="btn btn-primary flex-1">GETTING UP WITH THE JACKSONS</a>
 			<a href="./3" class="btn btn-primary flex-1">WHAT WAS GOING ON ON FRIDAY MORNING?</a>
--- src/routes/openclozes/[title]/+page.svelte
+++ src/routes/openclozes/[title]/+page.svelte
@@ -1,30 +1,28 @@
 <script lang="ts">
-	const input = `Mr. Jackson's father had just {passed away}. He had died of cancer. George had {sat up with} him during the last night at the hospital. Both men knew that the end was {drawing near} but the older man {kept on} talking cheerfully.
-"I am not unhappy," he told his son. "Sometimes life has been difficult, but I usually {faced up to} its problems. When I was a student, I {tried out for} the football team and I didn't meet with success. I {dropped out of} high school for a year after that. I {fell in with} some young men who were robbing people on the streets. We didn't use real guns. We {held} people {up} with wooden guns that looked real.
-The police arrested five of us and made us {line up} at the station. They {called in} people who had been robbed and asked them to {pick out} the robber. The boy who had done most of the robbing was not there. He had {got away}.
-"One of the people picked out another boy and me. But of course we didn't {let on} that we were guilty. I promised myself that if I {got out of} trouble, I'd {go back} to school and never steal again. I've never {gone back on} that promise.
-"I {met with} a lawyer several times, and at the trial the judge {let} me {off} with a warning. Back in school, I worked very hard and graduated, and then I {looked for} a job. A couple of years later I met your mother and we really {fell for} each other. I was ready to {settle down} and we got married.
-"Your mother always {stood by} me, even times were hard and food was scarce. When I needed help, she always gave it to me. She never {hung back} She was a wonderful woman, George. I {looked back on} our years of marriage with no regrets. Sara and I always {pulled together} like a team of - a team of good horses."
-He was getting tired and was beginning to {drift off}, but he tried to keep talking. His son could hardly {hold back} the tears.
-"She was a - was a wonderful - wonderful..." The old man's voice faded, and his breathing suddenly stopped. George {rang for} the nurse.`
-	let answers = input.split(/{|}/).filter((_, i) => i % 2 === 1)
-	let stored = localStorage.getItem("inputs0")?.split(",")
-	if (stored && stored.length < answers.length)
-		stored.push(...Array(answers.length - stored.length).fill(""))
-	let inputs = stored ?? answers.map((_) => "")
-	let live = !!localStorage.getItem("live")
-	$: inputs, live, save()
-	function clear() {
-		localStorage.removeItem("inputs0")
-		location.reload()
-	}
+	import { onMount } from "svelte"
+	import type { PageData } from "./$types"
+
+	export let data: PageData
+
+	let live = false
+
 	function check() {
 		live = !live
 	}
+
 	let show_answer_pls = false
 	function show_answer() {
 		show_answer_pls = !show_answer_pls
 	}
+
+	function clear() {
+		localStorage.removeItem("inputs0")
+		location.reload()
+	}
+
+	let answers = data.text.split(/{|}/).filter((_, i) => i % 2 === 1)
+	let stored: string[] = []
+
 	let autosave = ""
 	let last_save = 0
 	function save() {
@@ -36,15 +34,27 @@ He was getting tired and was beginning to {drift off}, but he tried to keep talk
 		localStorage.setItem("inputs0", inputs.join())
 		live ? localStorage.setItem("live", "true") : localStorage.removeItem("live")
 	}
+
+	let inputs: string[] = []
+
+	onMount(() => {
+		live = localStorage.getItem("live") !== null
+		stored = (localStorage.getItem("inputs0") ?? "").split(",")
+
+		if (stored.length < answers.length)
+			stored.push(...Array(answers.length - stored.length).fill(""))
+
+		inputs = stored ?? answers.map((_) => "")
+	})
 </script>
 
 <svelte:head>
-	<title>AN OLD MAN LOOKED BACK ON HIS LIFE</title>
+	<title>{data.title}</title>
 </svelte:head>
 
 <div class="absolute top-0 left-0 right-0 flex items-center flex-col px-4 pt-1 pb-10">
 	<div class="text-black dark:text-white leading-[2.5rem] w-full lg:w-[48rem]">
-		<div class="text-center text-3xl font-bold mt-12">AN OLD MAN LOOKED BACK ON HIS LIFE</div>
+		<div class="text-center text-3xl font-bold mt-12">{data.title}</div>
 		<div class="fixed top-0 left-0 right-0 flex justify-center">
 			<div class="m-1 px-3 rounded-md bg-white dark:bg-base-100">
 				<div class="flex sm:hidden flex-row justify-center gap-4 m-1">
@@ -170,7 +180,7 @@ He was getting tired and was beginning to {drift off}, but he tried to keep talk
 				</div>
 			</div>
 		</div>
-		{#each input.split(/{|}/) as text, i}
+		{#each data.text.split(/{|}/) as text, i}
 			{#if i % 2 === 0}
 				{#if text.includes("\n")}
 					{text.split("\n")[0]}
