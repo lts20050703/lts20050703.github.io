986a1d669d7ed83af3a5f0c12532ded383991fdb
lts20050703 <lts20050703@gmail.com>
2023-04-14 20:53:50 +0700

    2023.04.14.5: WIP: temp fix

--- src/lib/v.ts
+++ src/lib/v.ts
@@ -1,1 +1,1 @@
-export default "2023.04.14.4"
+export default "2023.04.14.5"
--- src/routes/2023/+page.svelte
+++ src/routes/2023/+page.svelte
@@ -18,7 +18,7 @@
 			target="_blank"
 			rel="noopener noreferrer">AZOTA</a
 		>
-		<a class="btn btn-primary" href="./sinh/3536">TEXT</a>
+		<!-- <a class="btn btn-primary" href="./sinh/3536">TEXT</a> -->
 	</div>
 	<span class="text-xl text-center">BÀI 37 38 CÁC ĐẶC TRƯNG CƠ BẢN CỦA QUẦN THỂ SINH VẬT</span>
 	<div class="flex flex-row gap-1">
@@ -28,7 +28,7 @@
 			target="_blank"
 			rel="noopener noreferrer">AZOTA</a
 		>
-		<a class="btn btn-primary" href="./sinh/3738">TEXT</a>
+		<!-- <a class="btn btn-primary" href="./sinh/3738">TEXT</a> -->
 	</div>
 	<span class="text-xl text-center">BÀI 39 BIẾN ĐỘNG SỐ LƯỢNG CÁ THỂ CỦA QUẦN THỂ SINH VẬT </span>
 	<div class="flex flex-row gap-1">
@@ -38,7 +38,7 @@
 			target="_blank"
 			rel="noopener noreferrer">AZOTA</a
 		>
-		<a class="btn btn-primary" href="./sinh/39">TEXT</a>
+		<!-- <a class="btn btn-primary" href="./sinh/39">TEXT</a> -->
 	</div>
 	<span class="text-xl text-center"
 		>BÀI 40 QUẦN XÃ SINH VẬT VÀ MỘT SỐ ĐẶC TRƯNG CƠ BẢN CỦA QUẦN XÃ SINH VẬT
@@ -50,6 +50,6 @@
 			target="_blank"
 			rel="noopener noreferrer">AZOTA</a
 		>
-		<a class="btn btn-primary" href="./sinh/40">TEXT</a>
+		<!-- <a class="btn btn-primary" href="./sinh/40">TEXT</a> -->
 	</div>
 </div>
