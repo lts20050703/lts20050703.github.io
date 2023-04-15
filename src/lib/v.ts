4a49e8053064bfdeeaba42f0feebd455aa96b35d
lts20050703 <lts20050703@gmail.com>
2023-04-15 09:59:24 +0700

    2023.04.15.3 SINH: BÀI 25: HỌC THUYẾT TIẾN HÓA ĐACUYN

--- src/lib/v.ts
+++ src/lib/v.ts
@@ -1,1 +1,1 @@
-export default "2023.04.15.2"
+export default "2023.04.15.3"
--- src/routes/2023/[subject]/+page.svelte
+++ src/routes/2023/[subject]/+page.svelte
@@ -22,6 +22,8 @@
 				</a>
 
 				<a href="./24" class="btn btn-primary">BÀI 24 CÁC BẰNG CHỨNG TIẾN HÓA</a>
+				<a href="./25" class="btn btn-primary">SINH: BÀI 25: HỌC THUYẾT TIẾN HÓA ĐACUYN</a>
+
 				<a href="./3536" class="btn btn-primary">BÀI 35 36</a>
 				<a href="./3738" class="btn btn-primary"
 					>BÀI 37 38 CÁC ĐẶC TRƯNG CƠ BẢN CỦA QUẦN THỂ SINH VẬT</a
