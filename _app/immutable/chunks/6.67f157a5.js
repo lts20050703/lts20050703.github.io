89976e1b746d8bd529e0e62bcdc00b8351ce5560
lts20050703 <lts20050703@gmail.com>
2023-04-16 15:09:02 +0700

    2023.04.16.3 Replace checkbox with toggle (larger hitbox)

---
+++ _app/immutable/chunks/6.67f157a5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/openclozes-page.svelte.3b584fae.js";export{t as component};
---
+++ _app/immutable/chunks/7.909a1df4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/openclozes-_title_-page.svelte.f7e649d1.js";export{t as component};
