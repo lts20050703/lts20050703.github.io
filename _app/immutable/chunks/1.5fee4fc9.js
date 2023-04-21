aee412d3871f566fd808d934f70deba37ade7def
lts20050703 <lts20050703@gmail.com>
2023-04-21 07:16:11 +0700

    2023.04.21.0 fix mark for review bug

--- _app/immutable/chunks/1.5fee4fc9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.af7e6426.js";export{t as component};
--- _app/immutable/chunks/7.c62c72f2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/openclozes-_title_-page.svelte.67a1a913.js";export{t as component};
