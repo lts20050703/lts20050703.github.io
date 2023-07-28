04225696685cb00441bd56400baa631e25956cff
lts20050703 <lts20050703@gmail.com>
2023-07-29 00:16:23 +0700

    Sync formatting with other repos to be consistent

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,4 @@
 import adapter from "@sveltejs/adapter-static"
 import { vitePreprocess } from "@sveltejs/kit/vite"
 /** @type {import('@sveltejs/kit').Config} */
-export default {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
-}
+export default { preprocess: vitePreprocess(), kit: { adapter: adapter() } }
