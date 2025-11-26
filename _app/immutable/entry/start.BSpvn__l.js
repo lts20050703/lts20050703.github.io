4335eb963281416b10ca148688775db0d0409c8f
Link <link20050703@gmail.com>
2025-11-26 03:51:40 -0800

    2025.11.26.0: Werewolf Moderator - Fix unable to import preset from URL - Cult Leader: Fix win check taking into account the cult leaders when it shouldn't - Win Check: Fix couple win condition not taking priority and supress players' roles win conditions. - Villager Win Check: Fix sage and soul not being taken into account when counting number of villages alive as well as checking if all are in couple. - Villager Win Check: Hoodlum, Soul, and Sage now longer get points if villager wins but not Hoodlum, Soul, or Sage wins. - Werewolf Win Check: Lone wolf now longer get points if werewolf wins but not lone wolf wins.
    Fixes: https://todo.sr.ht/~lts20050703/20050703.xyz/230

---
+++ _app/immutable/entry/start.BSpvn__l.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/C1azy2KX.js";export{o as load_css,r as start};
--- _app/immutable/entry/start.BkDQaMrD.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BZS5-PST.js";export{o as load_css,r as start};
