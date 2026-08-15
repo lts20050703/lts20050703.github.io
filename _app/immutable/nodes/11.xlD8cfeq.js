37b68c71778f6540d29661712afcb06854d54bea
Link <Link20050703@gmail.com>
2026-08-15 02:42:33 -0700

    2026.08.15.0: Optimize images (AVIF)

---
+++ _app/immutable/nodes/11.xlD8cfeq.js
@@ -0,0 +1,53 @@
+import{F as e,L as t,M as n,N as r,Q as i,X as a,Z as o,dt as s,k as c,ot as l,q as u,st as d,u as f,ut as p}from"../chunks/Dez-xeWK.js";import{a as m,t as h}from"../chunks/CRohWeFL.js";import{t as g}from"../chunks/CWk8zFoh.js";var _=e(`<div class="mt-4 flex flex-col gap-1 rounded bg-base-100 p-4"><div class="flex flex-row items-center gap-4"><img class="size-12 rounded-full" alt=""/> <div><div><b>Darius Shah</b> </div> <div class="text-base-content/50"> </div></div></div> <div><!></div></div>`);function v(e,t){var o=_(),l=a(o),d=a(l),p=i(d,2),m=a(p),h=i(a(m));s(m);var g=i(m,2),v=a(g,!0);s(g),s(p),s(l);var y=i(l,2),b=a(y);c(b,()=>t.children),s(y),s(o),u(()=>{f(d,`src`,`/darius/pfp/${t.pfp??``}.avif`),n(h,` ${t.feeling?`is feeling ${t.feeling}`:``}
+				${t.is_with?`with ${t.is_with}`:``}`),n(v,t.date)}),r(e,o)}var y=e(`Hi, I'm Darius, and welcome! This is my first ever post! <br/> I was born in Mumbai, and currently live with my parents in the outskirts of the city. We have a
+			lot of mango and jackfruit trees in the backyard!`,1),b=e(`This random girl that I met years ago somehow doesn't seem to get older just broke into my
+			house and claim she was the goddess of death, I was yelling at her until my son entered the
+			room. Mekana did nothing wrong to deserve death, <b><u>and I will not let that happen again to my son</u></b>`,1),x=e(`I have been chasing immortality my whole life, and yet, now that I invented it, I doesn't feel
+			as good as doing things I haven't done for years like watching a movie by myself... <br/> <i>Why?</i>`,1),S=e(`My son is moving out tomorrow. He is my son, but I'm not even sure if I'm his dad anymore. I
+			had been too focused on finding the cure of death and perhaps forgot to take care of him after
+			Mekana's death... <br/> I don't even know if he will ever come and visit me after everything he had went through...`,1),C=e(`The girl has come back to my house! I thought she came to take me to heaven, but I guess she
+			just want to say sorry, and I bursted out laughing 🤣 <br/> We talked a little bit about my life, and she seems to be confused why I don't want to use my invention,
+			which is understandable. I guess I'll just have to wait for her to come back to my house... <br/> My whole life I have been seeking a miracle - how to cheat death - and yet, it's already a miracle
+			that I live to this very day - each heartbeat, every breath, is a rejection of death.`,1),w=e(`<!> <div class="flex flex-col items-center gap-4"><div class="flex w-5xl max-w-full flex-col gap-1"><div class="relative mb-32"><img src="/darius/banner.avif" alt=""/> <img class="absolute -bottom-32 left-4 size-64 rounded-full border-4" src="/darius/pfp/10.avif" alt=""/></div> <div class="text-xl font-bold">Darius Shah</div> <div class="text-base-content/50">@dariusshah</div> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!></div> <hr class="w-5xl max-w-full opacity-25"/> <div class="prose mt-4 w-5xl max-w-full text-base-content"><h2 class="text-center">UnEssay #3 Write-up</h2> <div class="flex flex-col gap-4 indent-8"><div>Most stories are told from the third person perspective, with an anonymous narrator
+				narrating the story. In the case of "The many deaths of Laila Starr", having a third person
+				narrator allowed the author to tell a parallel story of Laila Starr and Darius Shah.
+				However, after reading some of my classmates' discussion posts, one thing that keeps being
+				mentioned is that the story can be confusing at times, due to the frequent jumping back and
+				forth in time, as well as jumping between Darius and Laila's point of view. Since I have
+				some experience with making websites in the past and often use social media, my thought was
+				“What if I transform the story from a third person view to focus on Darius, and you are
+				friends with him on Facebook?”.</div> <div>After the initial idea, I started writing, putting myself into Darius's shoes, like what
+				would he post to his Facebook when something important happens. After having a rough number
+				of posts, I start making the website, putting all the posts into it.</div> <div>If you use Facebook or any social media, you might notice that the timeline is sorted in
+				reverse, with oldest posts at the top instead of at the bottom, and this is intentional. In
+				Facebook's case, it makes sense to put the latest post at the top for easy viewing, but
+				because this is a story I'm trying to tell, it makes more sense to order the posts
+				chronologically.</div></div> <h3 class="text-center">Works Cited</h3> <div>V, Ram, et al. The Many Deaths of Laila Starr. Boom! Studios, 2024.</div></div></div>`,1);function T(e,n){d(n,!0);var c=w(),u=o(c);g(u,{get title(){return m.darius},get description(){return h.darius},uri:`darius`});var f=i(u,2),_=a(f),T=i(a(_),6);v(T,{date:`58 years ago`,pfp:0,children:(e,t)=>{p();var n=y();p(2),r(e,n)},$$slots:{default:!0}});var E=i(T,2);v(E,{date:`58 years ago`,feeling:`happy 😊`,pfp:1,children:(e,n)=>{p();var i=t(`Today I learned that my parents hired a very tall man to tend our garden, and his name is
+			Bardhan! We had so much fun talking together, he answered so many questions I asked!`);r(e,i)},$$slots:{default:!0}});var D=i(E,2);v(D,{date:`58 years ago`,feeling:`sad 😔`,pfp:1,children:(e,n)=>{p();var i=t(`Today I learned that Bardhan only had boiled rice, a stew, and a tumbler of milk to eat
+			throughout the day, and he wasn't even allowed to enter my house to use the bathroom or take a
+			shower. I asked my parents why don't we share some food with him, and they said it's because
+			they're not for him, and I shouldn't encourage him to think so. I don't get it, what do they
+			even mean? I felt bad so I secretly sneaked out of the house and gave him some, and I feel
+			good when he hugged me and said "Don't forget me, Darius"`);r(e,i)},$$slots:{default:!0}});var O=i(D,2);v(O,{date:`58 years ago`,feeling:`excited 😃`,pfp:2,children:(e,n)=>{p();var i=t(`We just moved to star garden height! I was so excited to go to school and meet new friends
+			today! We had so much fun playing Tennis and looking over the balcony!`);r(e,i)},$$slots:{default:!0}});var k=i(O,2);v(k,{date:`56 years ago`,feeling:`sad 😔`,pfp:3,children:(e,n)=>{p();var i=t(`After many years of not seeing Darius, I just found out that he has passed away. I haven't
+			seen him for many years now, but for some reason, now I miss him so much. I miss the time we
+			were still together, and he would carry me on his shoulder, answering my questions. I will
+			forever miss you Darius...`);r(e,i)},$$slots:{default:!0}});var A=i(k,2);v(A,{date:`46 years ago`,feeling:`angry 😡`,pfp:4,children:(e,n)=>{p();var i=t(`Me and my best friend Zapfar just had a big argument about my girlfriend Danika. My eyes are
+			still black and hurt like hell from the punch he gave me, I think he's just jealous over my
+			beautiful girlfriend.`);r(e,i)},$$slots:{default:!0}});var j=i(A,2);v(j,{date:`44 years ago`,feeling:`sad 😔`,pfp:5,children:(e,n)=>{p();var i=t(`Danika just broke up with me… I don't know what to do anymore… I still remember the time I had
+			a big argument with Zapfar about her, thinking that everything will eventually work out, but
+			here we are, it didn't work out after all, and I don't even know if Zapfar is still alive or
+			not after everything that happened…`);r(e,i)},$$slots:{default:!0}});var M=i(j,2);v(M,{date:`40 years ago`,feeling:`happy 😊`,pfp:5,is_with:`Menaka Shah`,children:(e,n)=>{p();var i=t(`Me and Menaka Shah just got married! We have been business partners for a few years now, and
+			I'm so happy to finally marry her!`);r(e,i)},$$slots:{default:!0}});var N=i(M,2);v(N,{date:`35 years ago`,feeling:`happy 😊`,pfp:5,is_with:`Menaka Shah`,children:(e,n)=>{p();var i=t(`We just have our first child! We're so happy to finally have a child together, and I can't
+			wait to see what the future holds for him!`);r(e,i)},$$slots:{default:!0}});var P=i(N,2);v(P,{date:`32 years ago`,feeling:`sad 😔`,pfp:5,children:(e,n)=>{p();var i=t(`I just found out that my wife Menaka has cancer and there's no cure for it… What do I even do
+			now… I'm not ready to see her gone… What am I going to do without her? Is there really nothing
+			I can do to save her?`);r(e,i)},$$slots:{default:!0}});var F=i(P,2);v(F,{date:`30 years ago`,feeling:`sad 😔`,pfp:6,children:(e,n)=>{p();var i=t(`Menaka passed away. It has been a fun and memorable 10 years of my life, but now it's only me
+			and my son...`);r(e,i)},$$slots:{default:!0}});var I=i(F,2);v(I,{date:`28 years ago`,feeling:`angry 😡`,pfp:7,children:(e,t)=>{p();var n=b();p(),r(e,n)},$$slots:{default:!0}});var L=i(I,2);v(L,{date:`15 years ago`,feeling:`happy 😊`,pfp:8,children:(e,t)=>{p();var n=x();p(3),r(e,n)},$$slots:{default:!0}});var R=i(L,2);v(R,{date:`15 years ago`,feeling:`sad 😔`,pfp:8,children:(e,t)=>{p();var n=S();p(2),r(e,n)},$$slots:{default:!0}});var z=i(R,2);v(z,{date:`2 years ago`,feeling:`sad 😔`,pfp:9,children:(e,n)=>{p();var i=t(`I have been diagnosed with cancer, and the doctor says I have a few months. I could always use
+			my invention to save myself, but I don't have the urge to use it for some reason. Perhaps I
+			have lived enough on this Earth, and my time has come?`);r(e,i)},$$slots:{default:!0}});var B=i(z,2);v(B,{date:`a week ago`,feeling:`happy 😊`,pfp:9,children:(e,t)=>{p();var n=C();p(4),r(e,n)},$$slots:{default:!0}});var V=i(B,2);v(V,{date:`yesterday`,feeling:`sad 😔`,pfp:9,children:(e,n)=>{p();var i=t(`This might be my last ever post. She insist that I use my own invention to save myself, but I
+			refused. I just called my son, and I have never been relived to hear him picking up. I told
+			him to come tomorrow, I need to tell him something, will see if I can even make it to tomorrow`);r(e,i)},$$slots:{default:!0}}),v(i(V,2),{date:`an hour ago`,feeling:`sad 😔`,pfp:10,children:(e,n)=>{p();var i=t(`Hi, this is Laila Starr writing. Darius has unfortunately passed away. There is nothing I can
+			do to save him, since he refused to use his immortality invention. Perhaps he has lived his
+			life to its fullest. Before he is gone forever, he allowed me to do whatever I want with his
+			immortality invention, and so I got rid of it. Who knows if I'm going to get my job back as
+			death, but I am not Darius, so this will be the last post.`);r(e,i)},$$slots:{default:!0}}),s(_),p(4),s(f),r(e,c),l()}export{T as component};
